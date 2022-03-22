5c7a0b3c0ac9f697721254415f7e71207067f5ec
beazy96 <96582724+beazy96@users.noreply.github.com>
2022-03-22 12:27:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    
+    for (int i = 0; i <=50; i++) {
+      if (i%2==0){
+      System.out.println(i);
+        }
+      }
+      for (int i1 = 50; i1 <=100; i1++) {
+        if (i1%2==1){
+          System.out.println(i1);
+      }
+      }
+}
+}
